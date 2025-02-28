43d4f87c0471ec64b9e5da2eb4d7ea55230fe2b4
lasse <77295879+lassejlv@users.noreply.github.com>
2025-02-28 17:09:50 +0100

    README.md: Add usage instructions; package.json: Add package name, version, bin, and build script; src/index.ts: Add error handling for missing GOOGLE_API_KEY.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 A simple little cli tool to generate ai generated commit messages!
 _this project is still in development, so expect bugs!_
+
+# Usage
+
+You must have git and bun installed on your system. Then run this command `bunx gen-ai-commit-message generate`
+_this will copy the ai generated message to your clipboard_
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ const command = process.argv[2]
 
 switch (command) {
   case 'generate': {
+    if (!Bun.env.GOOGLE_API_KEY) {
+      consola.error('Missing GOOGLE_API_KEY')
+      consola.info("You can set your key in a .env file or use this cmd: export GOOGLE_API_KEY='YOUR_KEY'\n\nmore models will be supported soon")
+      process.exit(1)
+    }
     GenerateCommand()
     break
   }
